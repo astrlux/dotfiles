448297f4e44c0686b729e36995f93ba8878bd714
astrlux <37908451+astrlux@users.noreply.github.com>
2022-12-20 20:59:34 -0800

    [nvim] remove guicursor

--- .config/nvim/lua/m/sets.lua
+++ .config/nvim/lua/m/sets.lua
@@ -2,10 +2,11 @@ local o             = vim.opt
 o.timeoutlen        = 450
 o.number            = true
 o.relativenumber    = true
+o.guicursor         = ''
 o.cursorline        = true
 o.signcolumn        = 'no'
 o.showcmd           = false
-o.showmode          = false
+o.showmode          = true
 o.showmatch         = false
 o.mouse             = 'a'
 o.ruler             = false
@@ -28,13 +29,6 @@ o.cmdheight         = 1
 o.shiftwidth        = 4
 o.softtabstop       = 4
 o.tabstop           = 4
-o.scrolloff         = 0
+o.scrolloff         = 9
 o.updatetime        = 50
 o.completeopt       = 'menuone,noselect'
-
-vim.api.nvim_exec([[
-    augroup RestoreCursorShapeOnExit
-        autocmd!
-        autocmd VimLeave * set guicursor=a:ver20
-    augroup END
-]], false)
