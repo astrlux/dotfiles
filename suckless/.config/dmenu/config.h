67b3dd4cc21608029c495dde72d344fcdd084aca
TheHiddenLayer <37908451+TheHiddenLayer@users.noreply.github.com>
2024-04-28 12:22:37 -0700

    update dmenu to nerd font patch

--- suckless/.config/dmenu/config.h
+++ suckless/.config/dmenu/config.h
@@ -7,20 +7,21 @@ static int min_width = 500;  /* minimum width when centered */
 static int fuzzy = 1;        /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "ConsolasLigaturizedv2:pixelsize=26:antialias=true:autohint=true",
+    "HackNerdFontMono:pixelsize=28:antialias=true:autohint=true",
     "Hack:pixelsize=28:antialias=true:autohint=true",
+    "ConsolasLigaturizedv2:pixelsize=26:antialias=true:autohint=true",
     "Consolas-Regular:pixelsize=30:antialias=true:autohint=true",
     "JetBrainsMonoNL-Regular:pixelsize=28:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#cccccc", "#1e1f29" },
-	[SchemeSel] = { "#ffffff", "#9755b3" },
-	[SchemeSelHighlight] = { "#d7d7d7", "#000000" },
-	[SchemeNormHighlight] = { "#ab76c2", "#000000" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeMid] = { "#d7d7d7", "#1f2026" },
+	[SchemeNorm] = { "#666666", "#19191a" },
+	[SchemeSel] = { "#ffffff", "#165668" },
+	[SchemeSelHighlight] = { "#ffffff", "#165668" },
+	[SchemeNormHighlight] = { "#ffffff", "#19191a" },
+    [SchemeMid] = { "#ffffff", "#19191a" },
+	[SchemeOut] = { "#ffffff", "#19191a" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
